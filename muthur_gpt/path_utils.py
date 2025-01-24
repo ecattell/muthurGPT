45a952e2e2cb25d372a88a6e87332485d92c7c01
KicuKicuKicu <134364778+KicuKicuKicu@users.noreply.github.com>
2025-01-24 23:11:51 +0100

    Update path_utils.py

--- muthur_gpt/path_utils.py
+++ muthur_gpt/path_utils.py
@@ -2,6 +2,7 @@ import os
 
 from muthur_gpt import constants
 
+
 class PathResolver():
     """
     Path resolver to resolve path to resources.
@@ -12,7 +13,7 @@ class PathResolver():
         lib_dir = os.path.dirname(os.path.abspath(__file__))
         self.project_root = os.path.normpath(os.path.join(lib_dir, ".."))
         # Order of path resolution:
-        self.resource_resolve_path =[
+        self.resource_resolve_path = [
             self._plugin_resource_root, self._base_resource_root]
 
     @property
@@ -36,7 +37,7 @@ class PathResolver():
         # Resolve order
         for resource_dir in self.resource_resolve_path:
             resource_path = os.path.join(resource_dir, relative_path)
-            #TODO make resource path case insensitive for name and ext?
+            # TODO make resource path case insensitive for name and ext?
             if os.path.exists(resource_path):
                 return resource_path
         return None
@@ -53,6 +54,11 @@ class PathResolver():
         name += constants.PROMPT_EXT
         return self._get_resource_path(os.path.join(constants.PROMPT_DIR, name))
 
-    def get_config_path(self):
+    def get_global_config_path(self):
         return os.path.join(
             self.project_root, constants.CONFIG_NAME + constants.CONFIG_EXT)
+
+    def get_plugin_config_path(self):
+        return os.path.join(
+            self.project_root, constants.PLUGIN_DIR, self.plugin_name,
+            constants.CONFIG_NAME + constants.CONFIG_EXT)
